771244b835db64b20f9daea4905addaaeca2145c
unknown <javiduran.scout@gmail.com>
2025-10-24 04:28:38 +0200

    new disposition of profile icon

--- app/src/main/java/com/example/littlelemon/components/Home.kt
+++ app/src/main/java/com/example/littlelemon/components/Home.kt
@@ -1,13 +1,9 @@
 package com.example.littlelemon.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,7 +11,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,38 +18,45 @@ import com.example.littlelemon.R
 
 @Composable
 fun Home(modifier: Modifier = Modifier, onNavigateProfile: () -> Unit) {
-    Column(
+    Box(
         modifier = modifier.fillMaxSize()
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.1f),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.End
+        Column(
+            modifier = modifier.fillMaxSize()
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Logo",
-                modifier = Modifier.size(200.dp)
-            )
-
-            Image(
-                painter = painterResource(id = R.drawable.profile),
-                contentDescription = "Profile",
+            Box(
                 modifier = Modifier
-                    .padding(start = 16.dp, end = 8.dp)
-                    .size(80.dp)
-                    .clickable(onClick = { onNavigateProfile() })
+                    .fillMaxWidth()
+                    .weight(0.1f),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "Logo",
+                    modifier = Modifier.size(200.dp)
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.6f)
+                    .padding(16.dp)
             )
         }
 
         Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(0.6f)
-                .padding(16.dp)
-        )
+            modifier = Modifier.align(alignment = Alignment.TopEnd)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "Profile",
+                modifier = Modifier
+                    .padding(top = 30.dp, end = 15.dp)
+                    .size(65.dp)
+                    .clickable(onClick = { onNavigateProfile() })
+            )
+        }
     }
 }
 
